a8404474752f060fb966b3b3b8b9897302bdd192
OmkarJavanjal <a96738@gmail.com>
2020-07-08 10:11:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -64,3 +64,76 @@ function sumSalaries(department) {
 }
 
 console.log(sumSalaries(company));
+
+//Linked list
+// The “delete element” and “insert element” operations are expensive. For instance, arr.unshift(obj) operation has to renumber all elements to make room for a new obj, and if the array is big, it takes time. Same with arr.shift().
+
+// The only structural modifications that do not require mass-renumbering are those that operate with the end of array: arr.push/pop. So an array can be quite slow for big queues, when we have to work with the beginning.
+
+// Alternatively, if we really need fast insertion/deletion, we can choose another data structure called a linked list.
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+let list = { value: 1 };
+list.next = { value: 2 };
+list.next.next = { value: 3 };
+list.next.next.next = { value: 4 };
+list.next.next.next.next = null;
+
+//Tasks:
+//Sum all numbers till the given one
+// Make 3 solution variants:
+
+// Using a for loop.
+// Using a recursion, cause sumTo(n) = n + sumTo(n-1) for n > 1.
+// Using the arithmetic progression formula.
+
+function sumTo(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log( sumTo(100) );
+
+function sumTo(n) {
+  if (n == 1) return 1;
+  return n + sumTo(n - 1);
+}
+
+console.log( sumTo(100) );
+
+function sumTo(n) {
+  return n * (n + 1) / 2;
+}
+
+console.log( sumTo(100) );
+
+//Calculate factorial
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+console.log( factorial(5) ); // 120
+//OR
+function factorial(n) {
+  return n ? n * factorial(n - 1) : 1;
+}
+
+console.log( factorial(5) ); // 120
+
+//Fibonacci numbers
