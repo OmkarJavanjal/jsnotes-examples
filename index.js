902d8910feb32eee4101d19e9c3ab3ec259a6532
OmkarJavanjal <a96738@gmail.com>
2020-07-08 09:57:26 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+
+//**************************************************************
+
 //Topic- Advanced working with functions
+
+//---------------------------------------------------------------//
 //6.1- Recursion and stack
 
 function pow(x, n) {
@@ -29,3 +34,33 @@ console.log( pow(2, 3) ); // 8
 function pow(x, n) {
   return (n == 1) ? x : (x * pow(x, n - 1));
 }
+
+//When a function makes a nested call, the following happens:
+// The current function is paused.
+// The execution context associated with it is remembered in a special data structure called execution context stack.
+// The nested call executes.
+// After it ends, the old execution context is retrieved from the stack, and the outer function is resumed from where it stopped.
+
+//Recursive traversals
+let company = { // the same object, compressed for brevity
+  sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+  development: {
+    sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+    internals: [{name: 'Jack', salary: 1300}]
+  }
+};
+
+// The function to do the job
+function sumSalaries(department) {
+  if (Array.isArray(department)) { // case (1)
+    return department.reduce((prev, current) => prev + current.salary, 0); // sum the array
+  } else { // case (2)
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep); // recursively call for subdepartments, sum the results
+    }
+    return sum;
+  }
+}
+
+console.log(sumSalaries(company));
