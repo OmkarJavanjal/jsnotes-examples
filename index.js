395fefe129c847c770c1b0c44d9f98629ed2c940
OmkarJavanjal <a96738@gmail.com>
2020-07-08 10:59:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -299,6 +299,42 @@ alert( Array.from(str) ); // H,e,l,l,o // Array.from converts an iterable into a
 //** */ Array.from operates on both array-likes and iterables.
 //** The spread syntax works only with iterables.
 
+//Get a new copy of an array/object
+let arr = [1, 2, 3];
+let arrCopy = [...arr]; // spread the array into a list of parameters
+                        // then put the result into a new array
+// do the arrays have the same contents?
+alert(JSON.stringify(arr) === JSON.stringify(arrCopy)); // true
+// are the arrays equal?
+alert(arr === arrCopy); // false (not same reference)
+// modifying our initial array does not modify the copy:
+arr.push(4);
+alert(arr); // 1, 2, 3, 4
+alert(arrCopy); // 1, 2, 3
+
+//Note that it is possible to do the same thing to make a copy of an object:
+let obj = { a: 1, b: 2, c: 3 };
+let objCopy = { ...obj }; // spread the object into a list of parameters
+                          // then return the result in a new object
+// do the objects have the same contents?
+alert(JSON.stringify(obj) === JSON.stringify(objCopy)); // true
+// are the objects equal?
+alert(obj === objCopy); // false (not same reference)
+// modifying our initial object does not modify the copy:
+obj.d = 4;
+alert(JSON.stringify(obj)); // {"a":1,"b":2,"c":3,"d":4}
+alert(JSON.stringify(objCopy)); // {"a":1,"b":2,"c":3}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
