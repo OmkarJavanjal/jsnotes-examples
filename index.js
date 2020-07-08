9c2f2ea612e765c7cff482f224b7ff202bc645b1
OmkarJavanjal <a96738@gmail.com>
2020-07-08 13:10:42 +0530

    1 file modified

--- index.js
+++ index.js
@@ -325,21 +325,133 @@ obj.d = 4;
 alert(JSON.stringify(obj)); // {"a":1,"b":2,"c":3,"d":4}
 alert(JSON.stringify(objCopy)); // {"a":1,"b":2,"c":3}
 
+//----------------------------------------------------------------------------------//
+//6.3- Variable scope
+//Code blocks
+//If a variable is declared inside a code block {...}, it’s only visible inside that block.
+{
+  // do some job with local variables that should not be seen outside
+  let message = "Hello"; // only visible in this block
+  alert(message); // Hello
+}
+alert(message); // Error: message is not defined
+
+
+{
+  // show message
+  let message = "Hello";
+  alert(message);
+}
+{
+  // show another message
+  let message = "Goodbye";
+  alert(message);
+}
+
+//***Please note, without separate blocks there would be an error, if we use let with the existing variable name: */
+// show message
+let message = "Hello";
+alert(message);
+// show another message
+let message = "Goodbye"; // Error: variable already declared
+alert(message);
+
+//For if, for, while and so on, variables declared in {...} are also only visible inside:
+if (true) {
+  let phrase = "Hello!";
+  alert(phrase); // Hello!
+}
+alert(phrase); // Error, no such variable!
+
+//Nested functions
+// A function is called “nested” when it is created inside another function.
+// It is easily possible to do this with JavaScript.
+function makeCounter() {
+  let count = 0;
+  return function() {
+    return count++;
+  };
+}
+let counter = makeCounter();
+alert( counter() ); // 0
+alert( counter() ); // 1
+
+//Task
+function Counter() {
+  let count = 0;
+  this.up = function() {
+    return ++count;
+  };
+  this.down = function() {
+    return --count;
+  };
+}
+let counter = new Counter();
+alert( counter.up() ); // 1
+alert( counter.up() ); // 2
+alert( counter.down() ); // 1
+
+
+/*let phrase = "Hello";
+if (true) {
+  let user = "John";
+  function sayHi() {
+    alert(`${phrase}, ${user}`);
+  }
+}
+sayHi();
+The result is an error.
+The function sayHi is declared inside the if, so it only lives inside it. There is no sayHi outside.
+*/
+
+function sum(a) {
+  return function(b) {
+    return a + b; // takes "a" from the outer lexical environment
+  };
+}
+alert( sum(1)(2) ); // 3
+alert( sum(5)(-1) ); // 4
+
+/*
+let x = 1;
+
+function func() {
+  console.log(x); // ReferenceError: Cannot access 'x' before initialization
+  let x = 2;
+}
+func();
+ */
+
+function inBetween(a, b) {
+  return function(x) {
+    return x >= a && x <= b;
+  };
+}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+
+function inArray(arr) {
+  return function(x) {
+    return arr.includes(x);
+  };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+function byField(fieldName){
+  return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+}
+users.sort(byField('name'));
+users.sort(byField('age'));
 
 
 
