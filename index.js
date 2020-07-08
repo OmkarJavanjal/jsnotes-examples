a1af9bee80a89a68ee7bdc49582f348e4ab3ae8c
OmkarJavanjal <a96738@gmail.com>
2020-07-08 09:45:58 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 //Topic- Advanced working with functions
 //6.1- Recursion and stack
+
+function pow(x, n) {
+  let result = 1;
+
+  // multiply result by x n times in the loop
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+console.log( pow(2, 3) ); // 8
+
+//Recursive thinking: simplify the task and call self:
+function pow(x, n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+console.log( pow(2, 3) ); // 8
+
+//shorter way
+function pow(x, n) {
+  return (n == 1) ? x : (x * pow(x, n - 1));
+}
